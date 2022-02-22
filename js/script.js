d3e058fda3388b44e98c4d0c765a867d358719b6
WashingtonYandun <washington.yandun06@gmail.com>
2022-02-21 23:25:53 -0500

    create a class for the effect

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//variables and constants
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// const DENSITY_SP = "         .:░▒▓█";
+// const DENSITY = "█▓Ñ@#W$98765543210?!abc;:+=-,._";
+
+let pic = new Image(); //user
+pic.src = "img/me.jpeg";
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+canvas.width = pic.width;
+canvas.height = pic.height;
+
+class ASCIIart {
+  #imageCellArray = [];
+  #density = [];
+  #pixels = [];
+  #ctx;
+  #width;
+  #height;
+
+  constructor(ctx, width, height) {
+    this.#ctx = ctx;
+    this.#width = width;
+    this.#height = height;
+    this.#ctx.drawImage(pic, 0, 0, this.#width, this.#height);
+    this.#pixels = this.#ctx.getImageData(0, 0, this.#width, this.#height);
+  }
+}
+
+const draw = () => {
+  const effect = new ASCIIart(ctx, pic.width, pic.height);
+  console.log(effect);
+  requestAnimationFrame(draw);
+};
+
+draw();
